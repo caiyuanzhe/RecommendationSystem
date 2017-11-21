53845ab040a35116c5b312656f1c477717a39ed9
caiyuanzhe <33817996+caiyuanzhe@users.noreply.github.com>
2017-11-21 11:52:31 +0800

    Update ZnbRecommendApplication.java

--- src/main/java/com/zhinengb/rs/ZnbRecommendApplication.java
+++ src/main/java/com/zhinengb/rs/ZnbRecommendApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * Recommendation System Application.
+ * 
+ * @author Yuanzhe Cai
+ *
+ */
 @SpringBootApplication
 @EnableScheduling
 public class ZnbRecommendApplication {
